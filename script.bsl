9c53fd13fc76d1455e1e8c9d96a95eebaa9a0c13
Miller <mmiller5@ilstu.edu>
2022-05-13 08:46:25 -0500

    Added function to shorten draw debug and added command for displaying

--- script.bsl
+++ script.bsl
@@ -4,7 +4,8 @@ func:
 	draw.drawLine(256, 356, 256, 156, (255, 255, 255))
 	draw.drawLine(156, 256, 356, 256, (255, 255, 255))
 	draw.drawCircle(256, 256, 100, (0, 255, 255))
-	draw.drawCircle(256, 256, 100, (0, 255, 255))
+	for(x in [0, 1, 2, 3, 4, 5]):
+		draw.drawCircle(256, 256, 100, (0, 255, 255))
 
 	if(testVar + " Miller" == "Max Miller" && testVar != "Max" || "test" != "test"):
 		print("hello Max!!!!")
--- testCode.py
+++ testCode.py
@@ -3,29 +3,47 @@ from os.path import exists
 from interpreter.utils.blcolors import blcolors
 from interpreter import main
 
-from tkinter import Tk, Canvas, PhotoImage
+try:
+    if sys.argv[2] == "--display":
+        display = True
+    else:
+        display = False
+except IndexError:
+    display = False
 
-WIDTH, HEIGHT = 512, 512
+if display:
+    from tkinter import Tk, Canvas, PhotoImage
 
-window = Tk()
-canvas = Canvas(window, width=WIDTH, height=HEIGHT, bg="#000000")
-canvas.pack()
-img = PhotoImage(width=WIDTH, height=HEIGHT)
-canvas.create_image((WIDTH/2, HEIGHT/2), image=img, state="normal")
+    WIDTH, HEIGHT = 512, 512
 
-def rgb_to_hex(rgb):
-    return '#%02x%02x%02x' % rgb
+    window = Tk()
+    canvas = Canvas(window, width=WIDTH, height=HEIGHT, bg="#000000")
+    canvas.pack()
+    img = PhotoImage(width=WIDTH, height=HEIGHT)
+    canvas.create_image((WIDTH/2, HEIGHT/2), image=img, state="normal")
+
+    def rgb_to_hex(rgb):
+        return '#%02x%02x%02x' % rgb
 
 def testCallback(cmd, data, **kwargs):
     if cmd == "error" or cmd == "debug":
             print(f"{blcolors.YELLOW}[{cmd}]: {data}{blcolors.CLEAR}")
     elif cmd == "draw":
-        for cord in data['cords']:
-            # print(cord)
-            img.put(
-                rgb_to_hex((data['color']['r'], data['color']['g'], data['color']['b'])), 
-                (cord['x'], cord['y'])
-            )
+        if display:
+            for cord in data['cords']:
+                # print(cord)
+                img.put(
+                    rgb_to_hex((data['color']['r'], data['color']['g'], data['color']['b'])), 
+                    (cord['x'], cord['y'])
+                )
+        else:
+            for x in range(len(data['cords'])):
+                data['cords'][x] = (data['cords'][x]['x'], data['cords'][x]['y'])
+            if len(data['cords']) > 10:
+                data['cords'] = data['cords'][0:10]
+                print(f"{blcolors.YELLOW}[{cmd}]: {data} {blcolors.MAGENTA}SHORTENED{blcolors.CLEAR}")
+            else: 
+                print(f"{blcolors.YELLOW}[{cmd}]: {data}{blcolors.CLEAR}")
     else:
         print(f"{blcolors.YELLOW}{blcolors.BOLD}[Callback]{blcolors.CLEAR}" + 
             f"{blcolors.YELLOW} [{cmd}]: {repr(data)}{blcolors.CLEAR}")
@@ -42,8 +60,8 @@ if __name__ == "__main__":
     if file_exists:
         file = open(fileName, "r")
         main.run(file, sendCommandCallback=testCallback)
-
-        window.mainloop()
+        if display:
+            window.mainloop()
     else:
         print(f"{blcolors.RED}Invalid filename: {repr(fileName)}{blcolors.CLEAR}")
 
